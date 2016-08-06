b69bdb4d1b17c8d1021f072cbfd5d824897cb737
Chandler <use.an.alias@gmail.com>
2016-08-06 14:18:48 -0700

    Add rule application system

--- ConvertToSLA/ConvertToSLA.py
+++ ConvertToSLA/ConvertToSLA.py
@@ -1,7 +1,12 @@
+import re
+
 from UM.Application import Application
 from UM.Extension import Extension
+from UM.Logger import Logger
 
 class ConvertToSLA(Extension):
+    RULES = []
+
     def __init__(self):
         super().__init__()
         Application.getInstance().getOutputDeviceManager().writeStarted.connect(self._convert)
@@ -10,4 +15,16 @@ class ConvertToSLA(Extension):
         scene = Application.getInstance().getController().getScene()
         if hasattr(scene, 'gcode_list'):
             gcode_list = getattr(scene, 'gcode_list')
-            print(gcode_list)
+            if gcode_list:
+                if ';SLA' not in gcode_list[0]:
+                    sla_gcode_list = self._convert_list(gcode_list)
+                    setattr(scene, 'gcode_list', sla_gcode_list) 
+                Logger.log('e', 'GCODE already in SLA format')
+
+    def _convert_list(gcode_list):
+        for rule in RULES:
+            try:
+                gcode_list = rule(gcode_list)
+            except Exeption as e:
+                Logger.log('e', 'A rule raised the exception {}'.format(str(e)))
+        return gcode_list
